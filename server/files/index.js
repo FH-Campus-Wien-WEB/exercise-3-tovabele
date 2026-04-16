26c35c286d0912f74647f0f7660038651ee1f9ef
Tova Bele <tova.bele@stud.hcw.ac.at>
2026-04-16 21:51:47 +0200

    first tasks > 1.4 not working server query

--- server/files/index.js
+++ server/files/index.js
@@ -1,92 +1,111 @@
 import { ElementBuilder, ParentChildBuilder } from "./builders.js";
 
 class ParagraphBuilder extends ParentChildBuilder {
-  constructor() {
-    super("p", "span");
-  }
+    constructor() {
+        super("p", "span");
+    }
 }
 
 class ListBuilder extends ParentChildBuilder {
-  constructor() {
-    super("ul", "li");
-  }
+    constructor() {
+        super("ul", "li");
+    }
 }
 
 function formatRuntime(runtime) {
-  const hours = Math.trunc(runtime / 60);
-  const minutes = runtime % 60;
-  return hours + "h " + minutes + "m";
+    const hours = Math.trunc(runtime / 60);
+    const minutes = runtime % 60;
+    return hours + "h " + minutes + "m";
 }
 
 function appendMovie(movie, element) {
-  new ElementBuilder("article").id(movie.imdbID)
-          .append(new ElementBuilder("img").with("src", movie.Poster))
-          .append(new ElementBuilder("h1").text(movie.Title))
-          .append(new ElementBuilder("p")
-              .append(new ElementBuilder("button").text("Edit")
-                    .listener("click", () => location.href = "edit.html?imdbID=" + movie.imdbID)))
-          .append(new ParagraphBuilder().items(
-              "Runtime " + formatRuntime(movie.Runtime),
-              "\u2022",
-              "Released on " +
-                new Date(movie.Released).toLocaleDateString("en-US")))
-          .append(new ParagraphBuilder().childClass("genre").items(movie.Genres))
-          .append(new ElementBuilder("p").text(movie.Plot))
-          .append(new ElementBuilder("h2").pluralizedText("Director", movie.Directors))
-          .append(new ListBuilder().items(movie.Directors))
-          .append(new ElementBuilder("h2").pluralizedText("Writer", movie.Writers))
-          .append(new ListBuilder().items(movie.Writers))
-          .append(new ElementBuilder("h2").pluralizedText("Actor", movie.Actors))
-          .append(new ListBuilder().items(movie.Actors))
-          .appendTo(element);
+    new ElementBuilder("article").id(movie.imdbID)
+        .append(new ElementBuilder("img").with("src", movie.Poster))
+        .append(new ElementBuilder("h1").text(movie.Title))
+        .append(new ElementBuilder("p")
+            .append(new ElementBuilder("button").text("Edit")
+                .listener("click", () => location.href = "edit.html?imdbID=" + movie.imdbID)))
+        .append(new ParagraphBuilder().items(
+            "Runtime " + formatRuntime(movie.Runtime),
+            "\u2022",
+            "Released on " +
+            new Date(movie.Released).toLocaleDateString("en-US")))
+        .append(new ParagraphBuilder().childClass("genre").items(movie.Genres))
+        .append(new ElementBuilder("p").text(movie.Plot))
+        .append(new ElementBuilder("h2").pluralizedText("Director", movie.Directors))
+        .append(new ListBuilder().items(movie.Directors))
+        .append(new ElementBuilder("h2").pluralizedText("Writer", movie.Writers))
+        .append(new ListBuilder().items(movie.Writers))
+        .append(new ElementBuilder("h2").pluralizedText("Actor", movie.Actors))
+        .append(new ListBuilder().items(movie.Actors))
+        .appendTo(element);
 }
 
 function loadMovies(genre) {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    const mainElement = document.querySelector("main");
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        const mainElement = document.querySelector("main");
 
-    while (mainElement.childElementCount > 0) {
-      mainElement.firstChild.remove()
+        while (mainElement.childElementCount > 0) {
+            mainElement.firstChild.remove()
+        }
+
+        if (xhr.status === 200) {
+            const movies = JSON.parse(xhr.responseText)
+            for (const movie of movies) {
+                appendMovie(movie, mainElement)
+            }
+        } else {
+            mainElement.append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
+        }
     }
 
-    if (xhr.status === 200) {
-      const movies = JSON.parse(xhr.responseText)
-      for (const movie of movies) {
-        appendMovie(movie, mainElement)
-      }
-    } else {
-      mainElement.append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
+    const url = new URL("/movies", location.href)
+    /* Task 1.4. Add query parameter to the url if a genre is given */
+    if (genre) {
+        url.searchParams.set("genre", genre)
     }
-  }
 
-  const url = new URL("/movies", location.href)
-  /* Task 1.4. Add query parameter to the url if a genre is given */
-
-  xhr.open("GET", url)
-  xhr.send()
+    xhr.open("GET", url)
+    xhr.send()
 }
 
 window.onload = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    const listElement = document.querySelector("nav>ul");
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        const navElement = document.querySelector("nav");
+        const listElement = document.querySelector("nav>ul");
 
-    if (xhr.status === 200) {
-      /* Task 1.3. Add the genre buttons to the listElement and 
-         initialize them with a click handler that calls the 
-         loadMovies(...) function above. */
-      const genres = JSON.parse(xhr.responseText);
+        if (xhr.status === 200) {
+            /* Task 1.3. Add the genre buttons to the listElement and 
+               initialize them with a click handler that calls the 
+               loadMovies(...) function above. */
+            const genres = JSON.parse(xhr.responseText);
+                console.log(genres)
 
-      /* When a first button exists, we click it to load all movies. */
-      const firstButton = document.querySelector("nav button");
-      if (firstButton) {
-        firstButton.click();
-      }
-    } else {
-      document.querySelector("body").append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
-    }
-  };
-  xhr.open("GET", "/genres");
-  xhr.send();
+            new ElementBuilder("li")
+                .append(new ElementBuilder('button')
+                .text("All Genres")
+                    .listener('click', () => loadMovies()))
+                .appendTo(navElement)
+
+            genres.forEach(genre => {
+                new ElementBuilder("button")
+                    .text(genre)
+                    .listener('click', () => loadMovies(genre))
+                    .appendTo(navElement)
+            })
+
+
+            /* When a first button exists, we click it to load all movies. */
+            const firstButton = document.querySelector("nav button");
+            if (firstButton) {
+                //firstButton.click();
+            }
+        } else {
+            document.querySelector("body").append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
+        }
+    };
+    xhr.open("GET", "/genres");
+    xhr.send();
 };
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const movieModel = require('./movie-model.js');
 const app = express();
 
 // Parse urlencoded bodies
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
@@ -15,42 +15,72 @@ app.use(express.static(path.join(__dirname, 'files')));
    This endpoint returns a sorted array of all the genres of the movies
    that are currently in the movie model.
 */
+app.get('/genres', function (req, res) {
+    console.log("attempting to find genres")
+    const genrelist = [];
+    let movies = Object.values(movieModel)
+    for (movie of movies) {
+        for (genres of movie.Genres) {
+            //console.log(genres)
+            genrelist.push(genres)
+
+        }
+    }
+    genrelist.sort()
+    //console.log(genrelist)
+    const uniqueGenres = []
+    for (item of genrelist) {
+        if (!uniqueGenres.includes(item)) {
+            uniqueGenres.push(item)
+        }
+    }
+    res.send(uniqueGenres);
+
+})
 
 /* Task 1.4: Extend the GET /movies endpoint:
    When a query parameter for a specific genre is given, 
    return only movies that have the given genre
  */
 app.get('/movies', function (req, res) {
-  let movies = Object.values(movieModel)
-  res.send(movies);
+    const genre = req.params.genre
+    //const exists = genre in movieModel
+
+    if (genre) {
+        res.send(movieModel[genre]);
+    }
+    else {
+        res.send(movieModel);
+    }
+//    let movies = Object.values(movieModel.genre)
 })
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
-  const id = req.params.imdbID
-  const exists = id in movieModel
- 
-  if (exists) {
-    res.send(movieModel[id])
-  } else {
-    res.sendStatus(404)    
-  }
+    const id = req.params.imdbID
+    const exists = id in movieModel
+
+    if (exists) {
+        res.send(movieModel[id])
+    } else {
+        res.sendStatus(404)
+    }
 })
 
-app.put('/movies/:imdbID', function(req, res) {
+app.put('/movies/:imdbID', function (req, res) {
 
-  const id = req.params.imdbID
-  const exists = id in movieModel
+    const id = req.params.imdbID
+    const exists = id in movieModel
 
-  movieModel[req.params.imdbID] = req.body;
-  
-  if (!exists) {
-    res.status(201)
-    res.send(req.body)
-  } else {
-    res.sendStatus(200)
-  }
-  
+    movieModel[req.params.imdbID] = req.body;
+
+    if (!exists) {
+        res.status(201)
+        res.send(req.body)
+    } else {
+        res.sendStatus(200)
+    }
+
 })
 
 app.listen(3000)
